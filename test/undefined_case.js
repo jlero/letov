cf7976ff92587655409076b3870947641b341710
mike <t30t40t50@gmail.com>
2017-01-11 16:26:45 +0200

    remove empty songsand recursion

--- test/undefined_case.js
+++ test/undefined_case.js
@@ -1,7 +1,7 @@
-const Letov = require('../lib/index')();
+const Letov = require('../lib/index');
 
 for (let i = 0; i < 10000; i += 1) {
-	if (Letov.random() === undefined) {
+	if (Letov() === undefined) {
 		throw 'Андефайнд найден.' + i
 	} 
 }
